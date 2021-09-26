31f28992e78e60ad0c2f43715572713ed71cfb87
AsyaCemeniel <asyaorlov89@gmail.com>
2021-09-26 14:34:25 +0300

    Add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import { Header } from "./components/header";
-import { MainPage } from "./pages";
+import { MainPage, Favorites } from "./pages";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MainPage />
+      <Switch>
+        <Route path="/simple-weather-site" exact={true} component={MainPage} />
+        <Route path="/favorites" exact={true} component={Favorites} />
+      </Switch>
     </div>
   );
 }
