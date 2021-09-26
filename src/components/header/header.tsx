67bf47f8d88d9e38c30e3714983cdb36ceaf4b30
AsyaCemeniel <asyaorlov89@gmail.com>
2021-09-26 14:45:48 +0300

    Add links to Header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { ThemeButton } from "../themeButton";
 import styles from "./header.module.scss";
+import star from "../../icons/star.svg";
 
 export const Header = () => {
   return (
     <header className={styles.container}>
       <div className={styles.main}>
-        <span className={styles.logo}>*Your Weather</span>
-        <div>
+        <Link to="/simple-weather-site" className={styles.logo}>
+          <span>*Your Weather</span>
+        </Link>
+        <div className={styles.control}>
+          <div className={styles.favorites}>
+            <img src={star} alt="star img" />
+            <Link to="/favorites">Favorites</Link>
+          </div>
           <ThemeButton />
         </div>
       </div>
