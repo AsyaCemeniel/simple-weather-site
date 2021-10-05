a400dd96e9edb3491178d7b1a812ae8385c4b58e
AsyaCemeniel <asyaorlov89@gmail.com>
2021-10-05 19:43:30 +0300

    Add custom hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   TypedUseSelectorHook,
   useDispatch as dispatchHook,
@@ -7,3 +8,29 @@ import { AppDispatch, AppThunk, RootState } from "./types";
 
 export const useDispatch = () => dispatchHook<AppDispatch | AppThunk>();
 export const useSelector: TypedUseSelectorHook<RootState> = selectorHook;
+
+export const useStateWithLocalStorage = (localStorageKey: string) => {
+  const [value, setValue] = useState(
+    localStorage.getItem(localStorageKey) || ""
+  );
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, value);
+  }, [value]);
+
+  return [value, setValue];
+};
+
+export const useDebounce = (value: string, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState<string>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
