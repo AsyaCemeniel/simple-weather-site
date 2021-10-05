8dfdf1ba43b6cf5029297f87c489b5420dc8a9f5
AsyaCemeniel <asyaorlov89@gmail.com>
2021-10-05 19:41:30 +0300

    Add SearchResponseType

--- src/types.ts
+++ src/types.ts
@@ -94,3 +94,19 @@ export type DailyResponseType = {
   MobileLink: string;
   Link: string;
 };
+
+export type SearchResponseType = {
+  Version: number;
+  Key: string;
+  Type: string;
+  Rank: number;
+  LocalizedName: string;
+  Country: {
+    ID: string;
+    LocalizedName: string;
+  };
+  AdministrativeArea: {
+    ID: string;
+    LocalizedName: string;
+  };
+};
