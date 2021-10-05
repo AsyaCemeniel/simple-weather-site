6b7e1267c93c8b3a4a5831721b5d070f511646c9
AsyaCemeniel <asyaorlov89@gmail.com>
2021-10-05 14:34:01 +0300

    Add DailyResponse type

--- src/types.ts
+++ src/types.ts
@@ -64,3 +64,33 @@ export type favoriteType = {
   minTemp: TempType;
   weatherText: string;
 };
+
+export type DailyResponseType = {
+  Date: string;
+  EpochDate: number;
+  Temperature: {
+    Minimum: {
+      Value: number;
+      Unit: string;
+      UnitType: number;
+    };
+    Maximum: {
+      Value: number;
+      Unit: string;
+      UnitType: number;
+    };
+  };
+  Day: {
+    Icon: number;
+    IconPhrase: string;
+    HasPrecipitation: boolean;
+  };
+  Night: {
+    Icon: number;
+    IconPhrase: string;
+    HasPrecipitation: boolean;
+  };
+  Sources: [string];
+  MobileLink: string;
+  Link: string;
+};
