2116d5af803c8a687f98ea96d1e8b6d37850c563
AsyaCemeniel <asyaorlov89@gmail.com>
2021-10-12 16:02:05 +0300

    Add dark and light mode switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { useSelector } from "./hooks";
 import { Header } from "./components/header";
 import { MainPage, Favorites } from "./pages";
 
 function App() {
+  const theme = useSelector((store) => store.ParametersReducer.theme);
+  const isDark = theme === "dark";
+
+  const currentTheme = isDark ? "theme-dark" : "theme-light";
+
   return (
-    <div className="App">
+    <div className={`color ${currentTheme}`}>
       <Header />
       <Switch>
         <Route path="/simple-weather-site" exact={true} component={MainPage} />
--- src/components/themeButton/themeButton.tsx
+++ src/components/themeButton/themeButton.tsx
@@ -1,17 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  useSelector,
+  useDispatch,
+  useStateWithLocalStorage,
+} from "../../hooks";
 import styles from "./themeButton.module.scss";
 import dark from "../../icons/space_moon.svg";
 import light from "../../icons/space_sun.svg";
+import { SET_THEME } from "../../redux/actions/parametersActions";
 
 export const ThemeButton = () => {
-  const [theme, setTheme] = useState(false);
+  const dispatch = useDispatch();
+  const [value, setValue] = useStateWithLocalStorage("theme");
 
-  const themeIcon = theme ? light : dark;
-  const themeClass = theme ? styles.dark : styles.light;
+  const theme = useSelector((store) => store.ParametersReducer.theme);
+  const isDark = theme === "dark";
+
+  const themeIcon = isDark ? light : dark;
+  const themeClass = isDark ? styles.dark : styles.light;
+
+  useEffect(() => {
+    if (value) {
+      dispatch({
+        type: SET_THEME,
+        payload: value,
+      });
+    }
+  }, []);
+
+  const switcher = () => {
+    dispatch({
+      type: SET_THEME,
+      payload: isDark ? "light" : "dark",
+    });
+    setValue(isDark ? "light" : "dark");
+  };
 
   return (
     <div className={themeClass}>
-      <img src={themeIcon} alt="theme img" onClick={() => setTheme(!theme)} />
+      <img src={themeIcon} alt="theme img" onClick={switcher} />
     </div>
   );
 };
